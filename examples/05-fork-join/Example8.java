fdcaab3e301084b60fe5ac18782f61d2a7b3e50a
Juan Alcantara <j.alejandrominaya@gmail.com>
2021-10-04 20:58:44 -0500

    feat: Add initial attempt for 8th parallelization

--- examples/05-fork-join/Example8.java
+++ examples/05-fork-join/Example8.java
@@ -19,6 +19,71 @@ public class Example8 extends RecursiveAction {
 	private static final int SIZE = 100_000;
 	private static final int MIN = 1_000;
 
+	private int array[], temp[], start, end;
+	
   // place your code here
+	public Example8 (int array[], int temp[], int start, int end) {
+		this.array = array;
+		this.temp = temp;
+		this.start = start;
+		this.end = end;
+	}
 
+	int count;
+	private void computeDirectly() {
+		for (int i = start; i < end; i++) {
+			count = 0;
+			for (int j = start; j < array.length; j++) {
+				if (
+						array[i] > array[j]
+						|| array[i] == array[j] && i < j
+				) {
+					count++;
+				}
+			}
+			temp[count] = array[i];
+		}
+	}
+
+	@Override
+	protected void compute() {
+		if ((start - end) <= MIN) {
+			computeDirectly();
+		} else {
+			int mid = (start + end)/2;
+			invokeAll(
+					new Example8(array, temp, start, mid),
+					new Example8(array, temp, mid, end)
+			);
+		}
+	}
+
+	public static void main (String args[]) {
+		long startTime, stopTime;
+		int array[] = new int[SIZE];
+		int temp[] = new int[SIZE];
+		double ms;
+		ForkJoinPool pool;
+
+		Utils.randomArray(array);
+		Utils.displayArray("before", array);
+
+		// Fill with 0s the temp array
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = 0;
+		}
+
+		System.out.printf("Starting with %d threads...\n", Utils.MAXTHREADS);
+		ms = 0;
+		for (int i = 1; i <= Utils.N; i++) {
+			pool = new ForkJoinPool(Utils.MAXTHREADS);
+			startTime = System.currentTimeMillis();
+			pool.invoke(new Example8(array, temp, 2, array.length));
+
+			stopTime = System.currentTimeMillis();
+			ms += (stopTime - startTime);
+		}
+		Utils.displayArray("after", temp);
+		System.out.printf("\navg time = %.5f ms\n", (ms / Utils.N));
+	}
 }
