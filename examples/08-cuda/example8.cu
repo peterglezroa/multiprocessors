d833006ccba312cc84b5cea9334e76bd96f6a2c3
Juan Alcantara <j.alejandrominaya@gmail.com>
2021-11-14 19:40:07 -0600

    feat: CUDA Enumeration sort

--- examples/08-cuda/example8.cu
+++ examples/08-cuda/example8.cu
@@ -17,6 +17,48 @@
 #include <cuda_runtime.h>
 #include "utils.h"
 
-#define SIZE 10000
+#define SIZE 100000
 #define THREADS 256
 #define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
+
+__global__ void sort(int *arr, int *res) {
+	int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+	for (int i = tid; i < SIZE; i += blockDim.x * gridDim.x) {
+		int n = 0;
+		for(int j = 0; j < SIZE; j++) {
+			if (arr[i] > arr[j] || arr[i] == arr[j] && i > j) {
+				n++;
+			}
+		}
+		res[n] = arr[i];
+	}
+}
+
+int main() {
+	int *arr, *res;
+	int *d_arr, *d_res;
+
+	arr = (int *)malloc(sizeof(int) * SIZE);
+	res = (int *)malloc(sizeof(int) * SIZE);
+	cudaMalloc((void**) &d_arr, sizeof(int) * SIZE);
+	cudaMalloc((void**) &d_res, sizeof(int) * SIZE);
+
+	printf("Starting...\n");
+	double ms = 0;
+	for (int i = 0; i < N; i++) {
+		random_array(arr, SIZE);
+		cudaMemcpy(d_arr, arr, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
+		start_timer();
+
+		sort<<<BLOCKS, THREADS>>>(d_arr, d_res);
+		ms += stop_timer();
+	}
+	cudaMemcpy(res, d_res, sizeof(int) * SIZE, cudaMemcpyDeviceToHost);
+	display_array("after", res);
+	printf("avg time = %.5lf ms\n", (ms / N));
+	cudaFree(d_arr);
+	cudaFree(d_res);
+	free(arr);
+	free(res);
+	return 0;
+}
